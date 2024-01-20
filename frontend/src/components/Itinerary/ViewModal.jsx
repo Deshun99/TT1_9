60558e24e5797ca9610fc328db4036da0599dab5
karinliow <karinliow@gmail.com>
2024-01-20 16:13:16 +0800

    Update ViewModal.jsx
    - remove unrequired codes
    - linked data with frontend

--- frontend/src/components/Itinerary/ViewModal.jsx
+++ frontend/src/components/Itinerary/ViewModal.jsx
@@ -14,36 +14,19 @@ import {
 import Paper from "@mui/material/Paper";
 import { Dialog, DialogContent, DialogActions } from "@mui/material";
 import * as React from "react";
+import { grey } from "@mui/material/colors";
 
 const ViewPopUp = ({ itineraryData }) => {
   const [open, setOpen] = React.useState(true);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
 
-  const boxStyle = {
-    position: "absolute",
-    top: "50%",
-    left: "50%",
-    transform: "translate(-50%, -50%)",
-    bgcolor: "background.paper",
-    width: "auto",
-    boxShadow: 24,
-  };
-
-  // todo: link api call here?
-
-  // const destination_detail = [
-  //   {
-  //     field: "itinerary_id"
-  //   }
-  // ]
-
+  const rows = itineraryData.destinations;
   return (
     <div>
       <Button onClick={handleOpen}>View</Button>
       <Dialog open={open} onClose={handleClose}>
         <DialogContent>
-          {/* <Box sx={boxStyle}> */}
           <Typography variant="h4" component="h3" align="center">
             Itinerary Details
           </Typography>
@@ -53,7 +36,7 @@ const ViewPopUp = ({ itineraryData }) => {
               required
               label="Itinerary Name"
               id="filled-required"
-              // value={itinerary_detail.itineraryTitle} // link data
+              value={itineraryData.title  } // link data
               variant="filled"
               InputProps={{
                 readOnly: true,
@@ -66,7 +49,7 @@ const ViewPopUp = ({ itineraryData }) => {
               required
               label="Budget ($)"
               id="filled-required"
-              // value={itinerary_detail.budget} // link data
+              value={itineraryData.budget} // link data
               variant="filled"
               InputProps={{
                 readOnly: true,
@@ -79,7 +62,7 @@ const ViewPopUp = ({ itineraryData }) => {
               required
               label="Country"
               id="filled-required"
-              // value={itinerary_detail.country} // link data
+              value={itineraryData.country} // link data
               variant="filled"
               InputProps={{
                 readOnly: true,
@@ -91,33 +74,26 @@ const ViewPopUp = ({ itineraryData }) => {
               <Table sx={{ minWidth: 650 }}>
                 <TableHead>
                   <TableRow>
-                    <TableCell>List of Destinations</TableCell>
+                    <TableCell>LIST OF DESTINATIONS</TableCell>
                   </TableRow>
                 </TableHead>
                 <TableBody>
-                  {/* {rows.map((row) => ( */}
+                  {rows.map((row) => (
                   <TableRow
-                    // key={row.name}
+                    key={row}
                     sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
                   >
                     <TableCell component="th" scope="row">
-                      {/* {row.name} */}
-                      destination 1
+                      {row}
                     </TableCell>
                   </TableRow>
-                  {/* ))} */}
+                  ))}
                 </TableBody>
               </Table>
             </TableContainer>
           </div>
         </DialogContent>
-        {/* </Box> */}
       </Dialog>
-      <DialogActions>
-        <Button autoFocus onClick={handleClose}>
-          Save changes
-        </Button>
-      </DialogActions>
     </div>
   );
 };
