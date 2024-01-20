becce5b94006aa792ab2a841f6a268d5783586b8
Deshun99 <e0560014@u.nus.edu>
2024-01-20 11:02:10 +0800

    new changes

--- backend/controllers/destination-controller.js
+++ backend/controllers/destination-controller.js
@@ -1,24 +1,24 @@
 const Destination = require("../model/Destination");
 
-const destination1 = async (req, res, next) => {
-  const { first_name, last_name, password, username } = req.body;
-  let existingUser;
-  try {
-    existingUser = await User.findOne({ username });
-  } catch (err) {
-    console.log(err);
-  }
+const destination = async (req, res, next) => {
+  const { cost, name, notes, country } = req.body;
+  // let existingUser;
+  // try {
+  //   existingUser = await Destination.findOne({ username });
+  // } catch (err) {
+  //   console.log(err);
+  // }
 
-  if (existingUser) {
-    return res.status(400).json({ message: "User already exists." });
-  }
+  // if (existingUser) {
+  //   return res.status(400).json({ message: "User already exists." });
+  // }
   // For security reasons, we do not store password in plain text
-  const hashedPassword = bcrypt.hashSync(password);
-  const user = new User({
-    first_name,
-    last_name,
-    password: hashedPassword,
-    username,
+  // const hashedPassword = bcrypt.hashSync(password);
+  const destination = new Destination({
+    cost,
+    name,
+    notes,
+    country,
   });
 
   try {
@@ -29,4 +29,4 @@ const destination1 = async (req, res, next) => {
   return res.status(201).json({ message: user });
 };
 
-exports.destination = destination1;
+exports.destination = destination;
