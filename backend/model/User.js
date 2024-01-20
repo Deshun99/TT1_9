5915ec840e3b1954520aa61405b3883cf88e0b3b
chandya <chandya.c99@gmail.com>
2024-01-20 09:47:34 +0800

    Update User entity to conform with ERD

--- backend/model/User.js
+++ backend/model/User.js
@@ -3,24 +3,23 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-    name: {
+    first_name: {
         type: String,
         required: true
     },
-    email: {
+    last_name: {
         type: String,
         required: true,
-        unique: true
     },
     password: {
         type: String,
         required: true,
         minlength: 6
     },
-    role: {
+    username: {
         type: String,
         required: true,
-        default: 'user'
+        unique: true
     }
 });
 
