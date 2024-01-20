737f6812e6ea08a6caa9e75d8aa5d3108541eeda
Deshun99 <e0560014@u.nus.edu>
2024-01-20 13:50:14 +0800

    get one destination

--- backend/routes/destination-routes.js
+++ backend/routes/destination-routes.js
@@ -4,7 +4,7 @@ const { createDestination, getDestination, editDestination, deleteDestination, g
 const dest_router = express.Router();
 
 dest_router.post("/createdestination", createDestination);
-dest_router.get("/getDestination", getDestination);
+dest_router.get("/getDestination/:id", getDestination);
 dest_router.get("/getAllDestinations", getAllDestinations);
 dest_router.put("/editDestination/:id", editDestination);
 dest_router.delete("/deleteDestination/:id", deleteDestination);
