26ee7bc4548c2ff889887aea77d5c29f7e05156c
chandya <chandya.c99@gmail.com>
2024-01-20 10:59:19 +0800

    Validate password length before hashing

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,7 @@ app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 app.use(express.json());
 app.use('/api', router);
 
-mongoose.connect(`mongodb+srv://admin:${process.env.MONGODB_PASSWORORD}@techtrek.gbctebp.mongodb.net/mern-auth?retryWrites=true&w=majority`).then(() => {
+mongoose.connect(`mongodb+srv://admin:${process.env.MONGODB_PASSWORD}@techtrek.gbctebp.mongodb.net/mern-auth?retryWrites=true&w=majority`).then(() => {
     app.listen(5000);
     console.log("Successfully connected to MongoDB! Listening to localhost 5000");
 })
