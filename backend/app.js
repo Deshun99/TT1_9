37e2dc2a09c3836ab1bf37452afdbcdd720b0290
Owen Tan <tanteongyuowen@gmail.com>
2024-01-20 10:37:04 +0800

    Add login

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const mongoose = require('mongoose')
-const router = require('./routes/user-routes');
+const userRouter = require("./routes/user-routes");
 
 const app = express();
 app.use(express.json());
-app.use('/api', router);
+app.use("/api", userRouter);
 
-mongoose.connect("mongodb+srv://admin:F55u19bSnV0I7xwo@techtrek.gbctebp.mongodb.net/mern-auth?retryWrites=true&w=majority").then(() => {
-    app.listen(5000);
-    console.log("Successfully connected to MongoDB! Listening to localhost 5000");
-})
-.catch((err) => console.log(err));
+mongoose
+  .connect(
+    `mongodb+srv://admin:${process.env.MONGODB_PASSWORORD}@cluster0.hes3x.mongodb.net/auth?retryWrites=true&w=majority`
+  )
+  .then(() => {
+    app.listen(5001);
+    console.log(
+      "Successfully connected to MongoDB! Listening to localhost 5000"
+    );
+  })
+  .catch((err) => console.log(err));
--- backend/routes/user-routes.js
+++ backend/routes/user-routes.js
@@ -1,8 +1,12 @@
 const express = require('express');
-const { signup } = require('../controllers/user-controller')
+const { signup, login } = require("../controllers/user-controller");
 
 const router = express.Router();
 
 router.post("/signup", signup);
+router.post("/login", login);
+// router.get("/user", verifyToken, getUser);
+// router.get("/refresh", refreshToken, verifyToken, getUser);
+// router.post("/logout", verifyToken, logout);
 
 module.exports = router;
