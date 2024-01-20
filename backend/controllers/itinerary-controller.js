7d17a97c3cda0e9a87419291555431a4c2c05085
chandya <chandya.c99@gmail.com>
2024-01-20 17:09:52 +0800

    add filter by country

--- backend/controllers/itinerary-controller.js
+++ backend/controllers/itinerary-controller.js
@@ -120,6 +120,32 @@ const retrieveItinerary = async (req, res) => {
     res.status(200).send({ itinerary: consolidatedItinerary });
 }
 
+const filterItineraryByCountry = async (req, res) => {
+    const { countryId } = req.params.id;
+
+    let itineraries;
+    try {
+        itineraries = await Itinerary.find({ country: countryId })
+            .populate({
+                path: 'country',
+                select: 'name',
+            })
+            .populate({
+                path: 'user',
+                select: 'first_name last_name',
+            });
+    } catch (e) {
+        console.log(e);
+    }
+
+    if (!itineraries || itineraries.length == 0) {
+        return res.status(400).send("Country has no itineraries");
+    }
+    console.log(itineraries.length);
+    return res.status(200).json(itineraries);
+
+}
+
 const updateItinerary = async (req, res) => {
     const findItinerary = await Itinerary.findById(req.params.id);
 
@@ -143,11 +169,11 @@ const getListofItineraryBasedOnDestination = async (req, res) => {
         const findDestinations = await ItineraryDestination.find({ destination_id: req.params.id });
 
         const listOfItineraries = [];
-        for(let i = 0; i < findDestinations.length; i++) {
+        for (let i = 0; i < findDestinations.length; i++) {
             const itinerary = await Itinerary.find({ itinerary_id: findDestinations[i].itinerary_id });
             listOfItineraries.push(itinerary);
         }
-        
+
         return res.status(201).json({ message: listOfItineraries });
     } catch (error) {
         console.error(error);
@@ -186,4 +212,5 @@ exports.deleteItinerary = deleteItinerary;
 exports.updateItinerary = updateItinerary;
 exports.retrieveItinerary = retrieveItinerary;
 exports.getListofItineraryBasedOnDestination =
-  getListofItineraryBasedOnDestination;
+    getListofItineraryBasedOnDestination;
+exports.filterItineraryByCountry = filterItineraryByCountry;
--- backend/routes/itinerary-routes.js
+++ backend/routes/itinerary-routes.js
@@ -6,15 +6,17 @@ const {
   retrieveItinerary,
   deleteItinerary,
   getListofItineraryBasedOnDestination,
+  filterItineraryByCountry
 } = require("../controllers/itinerary-controller");
 
-const router = express.Router();
+const itinerary_router = express.Router();
 
-router.post('/create', createItinerary);
-router.get('/retrieveAll/:userId', retrieveUserItineraries);
-router.get('/retrieveSingular/:itineraryId', retrieveItinerary);
-router.delete('/delete/:itineraryId', deleteItinerary);
-router.put("/update/:id", updateItinerary);
-router.get("/getListBasedOnDestinationId/", getListofItineraryBasedOnDestination);
+itinerary_router.post('/create', createItinerary);
+itinerary_router.get('/retrieveAll/:userId', retrieveUserItineraries);
+itinerary_router.get('/retrieveSingular/:itineraryId', retrieveItinerary);
+itinerary_router.delete('/delete/:itineraryId', deleteItinerary);
+itinerary_router.put("/update/:id", updateItinerary);
+itinerary_router.get("/getListBasedOnDestinationId/", getListofItineraryBasedOnDestination);
+itinerary_router.get("/filterItineraryByCountry/:id", filterItineraryByCountry);
 
-module.exports = router;
+module.exports = itinerary_router;
