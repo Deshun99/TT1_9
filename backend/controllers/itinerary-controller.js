7e3b5b6acb893073c20a37eb866c6c0826e71474
Darren Chee <gitdarrenchee@gmail.com>
2024-01-20 12:41:43 +0800

    completed create itinerary

--- backend/controllers/itinerary-controller.js
+++ backend/controllers/itinerary-controller.js
@@ -5,28 +5,44 @@ const Country = require('../model/Country');
 const User = require('../model/User');
 
 const createItinerary = async (req, res, next) => {
-    const { userId, itineraryTitle, budget, countryId, destinations } = req.body;
+    const { userId, itineraryTitle, budget, countryId, destinationIds } = req.body;
 
     if (!userId || !itineraryTitle || !countryId) {
         return res.status(400).send("Missing fields!");
     }
 
     let currentItinerary = new Itinerary({
-        user: User.findById(userId),
-        country: Country.findById(countryId),
+        user: userId,
+        country: countryId,
         budget,
         title: itineraryTitle
     });
 
     try {
-        await currentItinerary.save();
+        const itinerary = await currentItinerary.save();
+        for (let i = 0; i < destinationIds.length; i++) {
+            createItineraryDestination(itinerary._id, destinationIds[i]);
+        }
 
-        res.status(200).send({ message: "Success" });
+        res.status(201).send({ message: itinerary });
     } catch (e) {
-
+        console.log(e);
+        res.status(500).send({ message: e })
     }
 }
 
+async function createItineraryDestination(itinerary_id, destination_id) {
+    const itineraryDestination = new ItineraryDestination({
+        itinerary_id,
+        destination_id,
+    });
+
+    try {
+        await itineraryDestination.save();
+    } catch (e) {
+        return e
+    }
+}
 
 // pass the entire destination object as well
 const retrieveUserItineraries = async (req, res, next) => {
