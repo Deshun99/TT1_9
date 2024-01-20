41586b49307d824311d26fdbb180a09b7c56c43c
Deshun99 <e0560014@u.nus.edu>
2024-01-20 11:36:25 +0800

    find user, internal checking

--- backend/routes/user-routes.js
+++ backend/routes/user-routes.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const { signup } = require('../controllers/user-controller')
+const { signup, findUser } = require('../controllers/user-controller')
 
 const router = express.Router();
 
 router.post("/signup", signup);
+router.get("/findUser", findUser);
 
 module.exports = router;
