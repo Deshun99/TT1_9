37c7b4a7db4f7a25c54e079011e047deac022233
Darren Chee <gitdarrenchee@gmail.com>
2024-01-20 13:18:19 +0800

    complete retrieve itinerary

--- backend/controllers/itinerary-controller.js
+++ backend/controllers/itinerary-controller.js
@@ -46,16 +46,58 @@ async function createItineraryDestination(itinerary_id, destination_id) {
 
 // pass the entire destination object as well
 const retrieveUserItineraries = async (req, res, next) => {
-    const { userId } = req.body;
+    const { userId } = req.params;
 
+    let itineraries;
+    try {
+        itineraries = await Itinerary.find({ user: userId });
+    } catch (e) {
+        console.log(e);
+    }
+
+    if (!itineraries) {
+        res.status(400).send("User has no itineraries");
+    }
+
+    const consolidatedItineraries = [];
+    for (let i = 0; i < itineraries.length; i++) {
+        const itinerary = itineraries[i];
+        const country = await Country.findById(itinerary.country);
+        const itineraryDestinations = await ItineraryDestination.find({ itinerary_id: itinerary._id });
+
+        const destinations = [];
+
+        for (let j = 0; j < itineraryDestinations.length; j++) {
+            const itineraryDestination = itineraryDestinations[j];
+            const destinationDetails = await Destination.findById(itineraryDestination.destination_id);
+
+            destinations.push(destinationDetails);
+        }
+
+        const consolidatedItinerary = {
+            destinations,
+            country,
+            budget: itinerary.budget,
+            title: itinerary.title,
+        }
+
+        consolidatedItineraries.push(consolidatedItinerary);
+    }
+
+    res.status(200).send({ itineraries: consolidatedItineraries });
 }
 
+// only adding and removing of destinations
 const updateItinerary = async (req, res, next) => {
 
 }
 
 const deleteItinerary = async (req, res, next) => {
     const { itineraryId } = req.body;
+
+}
+
+async function deleteItineraryDestinations() {
 
 }
 
--- backend/routes/itinerary-routes.js
+++ backend/routes/itinerary-routes.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const { createItinerary } = require('../controllers/itinerary-controller');
+const { createItinerary, retrieveUserItineraries } = require('../controllers/itinerary-controller');
 
 const router = express.Router();
 
 router.post('/create', createItinerary);
+router.get('/retrieve/:userId', retrieveUserItineraries);
 
 module.exports = router;
